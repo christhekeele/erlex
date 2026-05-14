bebc6981771ec7e65ecfb0a989076592aba0464e
Chris Keele <christhekeele@gmail.com>
2026-05-14 10:53:01 -0500

    Try to remove possibly old dialyzer warnings (may be needed for older versions, we'll see).

--- dialyzer.ignore_warnings.exs
+++ dialyzer.ignore_warnings.exs
@@ -1,8 +1,1 @@
-[
-  {"lib/erlex.ex", :no_return, 63},
-  {"lib/erlex.ex", :no_return, 96},
-  {"lib/erlex.ex", :no_return, 139},
-  {"lib/erlex.ex", :no_return, 149},
-  {"lib/erlex.ex", :no_return, 168},
-  {"lib/erlex.ex", :no_return, 189}
-]
+[]
